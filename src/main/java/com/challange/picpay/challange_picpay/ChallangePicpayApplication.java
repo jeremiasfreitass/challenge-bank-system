da7df1527444e8a691a1a99a5144ef14b2eb1234
Jeremias Freitas <jeremias.freitas@dtidigital.com.br>
2025-05-19 00:14:03 -0300

    first commit

---
+++ src/main/java/com/challange/picpay/challange_picpay/ChallangePicpayApplication.java
@@ -0,0 +1,13 @@
+package com.challange.picpay.challange_picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangePicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallangePicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challange/picpay/challange_picpay/ChallangePicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challange.picpay.challange_picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallangePicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
