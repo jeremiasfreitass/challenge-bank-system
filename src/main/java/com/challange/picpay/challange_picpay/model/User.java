6a9a65c99969e2b2cbaee332bedb90e2b6da920a
Jeremias Freitas <jeremias.freitas@dtidigital.com.br>
2025-05-19 19:19:16 -0300

    user data models

--- src/main/java/com/challange/picpay/challange_picpay/model/User.java
+++ src/main/java/com/challange/picpay/challange_picpay/model/User.java
@@ -1,21 +1,32 @@
 package com.challange.picpay.challange_picpay.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.usertype.UserType;
 
 import java.math.BigDecimal;
 
-@Entity(name="user")
-@Table(name = "user")
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String firstName;
+
     private String lastName;
 
     @Column(unique = true)
-    private String CPF;
+    private String document;
 
     private String password;
 
@@ -23,5 +34,7 @@ public class User {
     private String email;
 
     private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
     private UserType userType;
 }
